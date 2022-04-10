914f1726d2530ee5b74565316e6674b299163b18
Antonin Deniau <antonin@playmoweb.com>
2022-04-10 12:01:37 +0200

    Add data

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,3 +1,43 @@
+<script>
+import DataSetCard from "./DataSetCard.vue";
+export default {
+  name: "DataList",
+  components: {
+    DataSetCard
+  },
+  data() {
+    return {
+      datasets: [
+        {
+          name: "The Metaverse LAND Dataset",
+          description: "The data is taken from Etherscan and Opensea and fed to a machine learning algorithm that is trained using past price movements and is retrained on a regular basis to keep the accuracy of the price predictions high. The core of the dataset are the aforementioned price predictions for each land that has enough trading history to compute a reasonable prediction.",
+          tags: ["nft", "metaverse", "ai", "virtual-real-estate", "the-sandbox", "land-valuations", "dao"]
+        },
+        {
+          name: "News Headlines",
+          description: "The latest headlines of some of the leading English newspapers, including headline text, date of publishing, URL, and other optional data like tags, language, etc. if available.2",
+          tags: ["news", "headlines", "new-york-times", "cnbc", "financial-times", "economist", "reuters", "sky-news", "bloomberg", "bbc"]
+        },
+        {
+          name: "Weather Germany 12/2021",
+          description: "This data set contains weather data of 69 selected locations (see sample file) in Germany in 12/2021 with an approximate interval of 15 minutes.",
+          tags: ["weather","germany","december","2021","temperature","wind","precipitation"]
+        },
+        {
+          name: "Store locations dm-drogerie markt",
+          description: "dm-drogerie markt is a chain of retail stores offering cosmetics, healthcare items, household products, health food and drinks in 13 countries (see wikipedia). This data set contains a list of all store locations and will be updateted regularly.",
+          tags: ["retail","cosmetics","healthcare","location","stores","retail", "household","food","drinks","germany"],
+        },
+        {
+          name: "UK Traffic 2'177'207 Accidents and Vehicles from 2005 to 2017",
+          description: "The UK government collects and publishes (usually on an annual basis) detailed information about traffic accidents across the country. This information includes, but is not limited to, geographical locations, weather conditions, type of vehicles, number of casualties and vehicle manoeuvres, making this a very interesting and comprehensive dataset for analysis and research.",
+          tags: ["automobiles","vehicles","transportation","law","games","demographics"],
+        }
+      ]
+    };
+  }
+}
+</script>
 <template class="mb-10">
 
 <div class="pb-10">
@@ -34,26 +74,11 @@
     </div>
 
       <div class="container mx-auto grid grid-cols-3 gap-3 col-span-4 pl-5">
-        <DataSetCard/>
-        <DataSetCard/>
-        <DataSetCard/>
-        <DataSetCard/>
+        <DataSetCard v-for="dataset in datasets" :tags="dataset.tags" :description="dataset.description" :name="dataset.name"/>
       </div>
   </div>
 
 </div>
 </template>
-
-<script>
-import DataSetCard from "./DataSetCard.vue";
-export default {
-  name: "DataList",
-  components: {
-    DataSetCard
-  }
-}
-</script>
-
 <style scoped>
-
 </style>
--- src/components/DataSetCard.vue
+++ src/components/DataSetCard.vue
@@ -1,33 +1,19 @@
-<template>
-    <card class="card card-compact w-82 pt-3 bg-gray-300 text-primary shadow-xl ">
-      <h2 class="card-title mx-2.5 px-3">{{ DataSets.Name }}</h2>
-      <span class="card-body pt-3">{{ DataSets.Description }}</span>
-      <div class="card-actions justify-end py-2 px-2">
-        <div v-for="Tag in DataSets.Tags" class="badge badge-outline badge-accent">{{ Tag }}</div>
-      </div>
-      <button class="btn btn-secondary">"More Info"</button>
-    </card>
-
-</template>
-
 <script>
 export default {
   name: "DataSetCard",
-  data() {
-    return {
-      DataSets:{
-        Name: 'Data Test I',
-        Description : "Nam erat diam, dapibus ac sodales in, maximus eget lacus. Suspendisse iaculis suscipit efficitur. Aenean feugiat, ex ac mollis consectetur, sapien odio malesuada diam, et volutpat elit metus sit amet libero. Suspendisse id quam metus. Ut pulvinar turpis id nibh finibus, vel ornare elit sollicitudin.",
-        Tags: ['Tag 1', "Tag 2", "Tag 3"]
-      },
-      Bonjour:"Coucou",
-    }
-  }
+  props: ['name', 'description', 'tags'],
 }
-
 </script>
-
-
+<template>
+    <card class="card card-compact w-82 pt-3 bg-gray-300 text-primary shadow-xl ">
+      <h2 class="card-title mx-2.5 px-3">{{ name }}</h2>
+      <span class="card-body pt-3">{{ description }}</span>
+      <div class="card-actions justify-end py-2 px-2">
+        <div v-for="tag in tags" class="badge badge-outline badge-accent">{{ tag }}</div>
+      </div>
+      <button class="btn btn-secondary">"More Info"</button>
+    </card>
+</template>
 <style scoped>
 a {
   color: #42b983;

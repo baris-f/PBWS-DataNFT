9683edd6260d889807bc68fd73f036488188a640
Matthieu BOSCHER <matthieu.boscher@epitech.digital>
2022-04-10 12:17:34 +0200

    data pres

--- src/components/DataSetPresentation.vue
+++ src/components/DataSetPresentation.vue
@@ -1,10 +1,20 @@
 <template>
-
+  <h1>{{DataSets.Name}}</h1>
 </template>
 
 <script>
 export default {
-  name: "DataSetPresentation"
+  name: "DataSetPresentation",
+  data() {
+    return {
+      DataSets:{
+        Name: 'Data Test I',
+        Description : "Nam erat diam, dapibus ac sodales in, maximus eget lacus. Suspendisse iaculis suscipit efficitur. Aenean feugiat, ex ac mollis consectetur, sapien odio malesuada diam, et volutpat elit metus sit amet libero. Suspendisse id quam metus. Ut pulvinar turpis id nibh finibus, vel ornare elit sollicitudin.",
+        Tags: ['Tag 1', "Tag 2", "Tag 3"]
+      },
+      Bonjour:"Coucou",
+    }
+  }
 }
 </script>
 
